e525069659be4c75b279fe6c4938bd582a73128b
nasichunamin <nasichunamin004@gmail.com>
2022-06-06 11:40:00 +0700

    setup axios

---
+++ src/Networks/apis/index.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import CONST from "../../utils/Constant";
+
+const { baseApi } = CONST;
+
+const config = {
+    baseURL: baseApi,
+    // headers: {
+    //   "app-id": "624a11d46b0e26728639c458",
+    //   "access-token": accessToken
+    // }
+}
+
+export const axiosInstance = axios.create(config);
--- src/pages/Citizen/Home/index.jsx
+++ src/pages/Citizen/Home/index.jsx
@@ -1,5 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { axiosInstance } from "../../../Networks/apis";
 
 export default function Home() {
-  return <div>Home</div>;
+
+  // const [data, setData] = useState([]);
+
+  // useEffect(() => {
+  //   console.log("fetching data test ");
+
+  //   axiosInstance.get("/facility")
+  //   .then((res) => {
+  //     console.log("Then")
+  //     console.log(res.data.data)
+  //     setData(res.data.data)
+  //   })
+  //   .catch((error) => {
+  //     console.log("Masuk ke Catch");
+  //     console.log(error);
+  //   });
+  // }, []);
+
+
+
+  return (
+    <div>
+      <div>Home</div>
+      {/* <div className="getData">
+      <h1>Axios Fetching</h1>``
+      {data.map((value, idx) => (
+        <h3 key={idx}>{value.facility_name}</h3>
+      )
+      )}
+    </div> */}
+    </div>
+  );
 }
