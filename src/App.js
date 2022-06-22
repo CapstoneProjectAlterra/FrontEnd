5d2d2cb55d650c6ac04068fa61c7fa1e23b647b6
Laili <amalialaili31@gmail.com>
2022-06-22 19:39:19 +0700

    add component alert

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import "./App.css";
 import RouteList from "./routes";
-import Alertt from "./components/Alert";
-import MainNavbar from "./components/MainNavbar";
-import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      {/* <RouteList /> */}
-      <MainNavbar />
-      <Alertt />
-      <Footer />
+      <RouteList />
     </>
   );
 }
