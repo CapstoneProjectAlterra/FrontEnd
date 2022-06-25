4bc21501a7d007c0211e0e7c3b1a4236cf2bbbf1
Muhammad Rafli <rafliogun49@gmail.com>
2022-06-25 23:59:49 +0700

    fixing overflow text in location. so i implementing nested row in there

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,5 +3,6 @@ import CustomInput from "./CustomInput";
 import TicketModal from "./TicketModal";
 import TicketCard from "./TicketCard";
 import TicketList from "./TicketList";
+import Footer from "./Footer";
 
-export { CustomButton, CustomInput, TicketModal, TicketCard, TicketList };
+export {CustomButton, CustomInput, TicketModal, TicketCard, TicketList, Footer};
