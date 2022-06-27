3d6aa455ae4e7c9c753038569d1a06893a74525b
Achmad Solehuddin <achmadsolehuddin23@gmail.com>
2022-06-27 11:25:24 +0700

    setup api, cookies, auth helper and news api

--- src/pages/Citizen/Home/index.jsx
+++ src/pages/Citizen/Home/index.jsx
@@ -1,14 +1,26 @@
-import React, {useEffect, useState} from "react";
-import {useDispatch, useSelector} from "react-redux";
-import {axiosInstance} from "../../../networks/apis";
-import {Col, Row} from "antd";
-import {CustomAlert, CustomButton, CustomInput} from "../../../components";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import axiosInstance from "../../../networks/apis";
+import { Col, Row } from "antd";
+import { CustomAlert, CustomButton, CustomInput } from "../../../components";
+import Cookies from "js-cookie";
+import axios from "axios";
 export default function Home() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    console.log("fetching data test ");
+    axios
+      .get(
+        "https://newsapi.org/v2/everything?q=vaksin%20covid&categories=health&sortBy=publishedAt&apiKey=68864b6a0889466db4e38b081263b981"
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
 
+    console.log("fetching data test ");
     axiosInstance
       .get("/facility")
       .then((res) => {
@@ -35,6 +47,7 @@ export default function Home() {
           <h3 key={idx}>{value.facility_name}</h3>
         ))}
       </div>
+      {/* {description.slice(0, 200) + (description.length > 200 ? '...' : '')} */}
       <CustomButton variant="primary">Primary</CustomButton>
       <CustomButton variant="secondary">Secondary</CustomButton>
       <CustomInput placeholder="test" />
