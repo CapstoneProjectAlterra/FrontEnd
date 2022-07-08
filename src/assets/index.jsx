cd960cb91e1829c80a46c65cff0c7bb3c09b4a31
Achmad Solehuddin <achmadsolehuddin23@gmail.com>
2022-07-08 18:20:09 +0700

    Add ticket not found and fix fetching ticket

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -3,5 +3,13 @@ import LogoPrimary from "./logo/logo-primary.png";
 import LogoSecondary from "./logo/logo-secondary.png";
 import Ilustration404 from "./illustration/not-found-vaccine.png";
 import SuntikVaksin from "./illustration/suntik-vaksin.png";
+import TicketNotFound from "./illustration/ticket-not-found.png";
 
-export { LogoPrimary, LogoSecondary, imgLogin, Ilustration404, SuntikVaksin };
+export {
+  LogoPrimary,
+  LogoSecondary,
+  imgLogin,
+  Ilustration404,
+  SuntikVaksin,
+  TicketNotFound,
+};
