cecca955d3fb63e2e36c384c58c71a1bd909804f
TakayumAja <asraftakayuma76@gmail.com>
2022-07-11 09:14:34 +0700

    API Integration - crud vaccine features

--- src/components/VaccineModalAdd/index.jsx
+++ src/components/VaccineModalAdd/index.jsx
@@ -5,28 +5,70 @@ import React, { useState } from "react";
 import CustomButton from "../CustomButton";
 
 /** Antd Design */
-import { Form, Modal, Select, InputNumber } from "antd";
+import { Form, Modal, Select, InputNumber, message } from "antd";
 
 /** Style */
 import style from "./VaccineModalAdd.module.css";
+import axiosInstance from "../../networks/apis";
+import { getUserId } from "../../utils/helpers/Auth";
+import { useEffect } from "react";
 
-const VaccineModalAdd = () => {
+const VaccineModalAdd = ({ refetchToggle, setRefetchToggle }) => {
   const [visible, setVisible] = useState(false);
   const [confirmLoading, setConfirmLoading] = useState(false);
   const [form] = Form.useForm();
+  const [vaccine, setVaccine] = useState([]);
+  const [facility, setFacility] = useState([]);
+
+  useEffect(() => {
+    axiosInstance
+      .get("/facility", { data: "" })
+      .then((response) => {
+        setFacility(response.data.data.filter((item) => item.profile.user_id === getUserId())[0]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axiosInstance
+      .get("/vaccine", { data: "" })
+      .then((response) => {
+        setVaccine(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
 
   const showModal = () => {
     setVisible(true);
   };
 
   const handleOk = (values) => {
+    const inputData = {
+      facility_id: facility.id,
+      vaccine_id: values.vaccine_id,
+      stock: values.kuota,
+    };
     setConfirmLoading(true);
-    console.log(values);
-    setTimeout(() => {
-      setVisible(false);
-      setConfirmLoading(false);
-      form.resetFields();
-    }, 2000);
+
+    axiosInstance
+      .post("/stock", inputData)
+      .then((response) => {
+        console.log(response);
+        setConfirmLoading(false);
+        setVisible(false);
+        setRefetchToggle(!refetchToggle);
+        form.resetFields();
+        message.success("Data berhasil ditambahkan");
+      })
+      .catch((error) => {
+        console.log(error);
+        setConfirmLoading(false);
+        message.error("Data gagal ditambahkan");
+      });
   };
 
   const handleCancel = () => {
@@ -95,9 +137,11 @@ const VaccineModalAdd = () => {
             ]}
           >
             <Select showSearch placeholder="Select Vaccine" optionFilterProp="children" onChange={onChange} onSearch={onSearch} filterOption={(input, option) => option.children.toLowerCase().includes(input.toLowerCase())}>
-              <Select.Option value="1">Sinovac</Select.Option>
-              <Select.Option value="2">Astra Zeneca</Select.Option>
-              <Select.Option value="3">Moderna</Select.Option>
+              {vaccine.map((item) => (
+                <Select.Option key={item.id} value={item.id}>
+                  {item.vaccine_name}
+                </Select.Option>
+              ))}
             </Select>
           </Form.Item>
           <Form.Item
