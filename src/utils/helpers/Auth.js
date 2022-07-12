aed2e7bc15e9a9cfc116099553884efd4fdbfa92
Achmad Solehuddin <achmadsolehuddin23@gmail.com>
2022-07-12 17:09:48 +0700

    Add isProfileNull helpers and implement it in Home page

--- src/utils/helpers/Auth.js
+++ src/utils/helpers/Auth.js
@@ -1,4 +1,5 @@
 import Cookies from "js-cookie";
+import axiosInstance from "../../networks/apis";
 
 export function getToken() {
   return Cookies.get("token");
@@ -32,4 +33,35 @@ export function isAuthenticatedAdmin() {
     }
   }
   return false;
+}
+
+export async function isProfileNull() {
+  const userId = getUserId();
+  const user = await axiosInstance
+    .get(`/user/${userId}`, { data: "" })
+    .then((res) => res.data.data)
+    .catch((err) => {
+      console.log(err);
+    });
+
+  const family = await axiosInstance
+    .get(`/family`, { data: "" })
+    .then((res) =>
+      res.data.data.filter((family) => family.profile.user_id === userId)
+    )
+    .catch((err) => {
+      console.log(err);
+    });
+
+  const profile = family.filter((family) => family.nik === user.username)[0];
+
+  const isNullish = Object.values(profile).some((value) => {
+    if (value === null || value === undefined || value === "") {
+      return true;
+    } else {
+      return false;
+    }
+  });
+
+  return isNullish;
 }
