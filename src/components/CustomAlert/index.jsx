1a089fbf503c88b192718c09f0b57741aab08279
Muhammad Rafli <rafliogun49@gmail.com>
2022-07-12 10:26:32 +0700

    adding navigate in custom alert to profile page

--- src/components/CustomAlert/index.jsx
+++ src/components/CustomAlert/index.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import style from "./Alert.module.css";
 import {Alert, Space} from "antd";
 import CustomButton from "../CustomButton";
+import {useNavigate} from "react-router-dom";
 
 function Alertt() {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    return navigate("/profile");
+  };
   return (
     <>
       <Alert
@@ -12,7 +17,9 @@ function Alertt() {
         type="info"
         action={
           <Space align="baseline" direction="vertical" style={{padding: "10px 0"}}>
-            <CustomButton variant="primary">Lengkapi Profilmu</CustomButton>
+            <CustomButton variant="primary" onClick={handleClick}>
+              Lengkapi Profilmu
+            </CustomButton>
           </Space>
         }
         closable
