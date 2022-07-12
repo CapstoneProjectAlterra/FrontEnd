0c50fc519c6249fb45aba0d69ef6e56bad265b34
Muhammad Rafli <rafliogun49@gmail.com>
2022-07-12 10:20:42 +0700

    creating alert for successful after booking vaccine and warning alert for remind user to fill the profile and confirming booking vaccine

--- src/components/SubmitFormButton/index.jsx
+++ src/components/SubmitFormButton/index.jsx
@@ -6,19 +6,25 @@ import {Navigate, useNavigate} from "react-router-dom";
 export default function SubmitFormButton(props) {
   const [isModalVisible, setIsModalVisible] = useState(false);
   const navigate = useNavigate();
-
+  // munculin modal
   const showModal = () => {
     setIsModalVisible(true);
   };
-
+  //   navigate ke ticket kalo buttonnya diklik
   const handleOk = () => {
     navigate("/ticket");
   };
 
   return (
     <>
-      <CustomButton variant="primary" block style={{marginTop: "24px"}} onClick={showModal}>
-        {props.children}
+      <CustomButton
+        variant="primary"
+        type="submit"
+        style={{height: "56px"}}
+        htmlType="submit"
+        onClick={showModal}
+      >
+        Pesan Vaksinasi
       </CustomButton>
       <SuccessAlert visible={isModalVisible} onOk={handleOk} />
     </>
--- src/components/WarningAlert/index.jsx
+++ src/components/WarningAlert/index.jsx
@@ -1,5 +1,83 @@
-import React from "react";
+import React, {useState} from "react";
+import {Button, Modal, Row, Col} from "antd";
+import styles from "./warningalert.module.css";
+import CustomButton from "../CustomButton";
+import {useNavigate} from "react-router-dom";
+import {WarningIcon} from "../../assets";
 
-export default function WarningAlert() {
-  return <div>WarningAlert</div>;
+export default function WarningAlert({type}) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const navigate = useNavigate();
+  const typeMode = [
+    // ini data buat yang type reminder
+    {
+      id: 1,
+      title: "Duh, data dirimu masih belum lengkap!",
+      desc: "Yuk, lengkapi data profilmu terlebih dahulu sebelum kembali melanjutkan pemesanan.",
+      cta: "Lengkapi profil",
+    },
+    // ini data yang type confirm
+    {
+      id: 2,
+      title: "Apakah kamu ingin melanjutkan pemesanan?",
+      desc: "Jangan lupa pastikan pesananmu telah sesuai sebelum melanjutkan.",
+      cta: "Lanjutkan",
+    },
+  ];
+
+  const data = type === "reminder" ? typeMode[0] : typeMode[1];
+
+  // munculin modal
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  //   navigate ke ticket kalo buttonnya diklik
+  const handleOk = () => {
+    if (type === "reminder") {
+      navigate("/profile");
+    } else if (type === "confirm") {
+      //   ini buat action yang type confirm
+    }
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  return (
+    <>
+      <CustomButton
+        variant="primary"
+        type="submit"
+        style={{height: "56px"}}
+        htmlType="submit"
+        onClick={showModal}
+      >
+        test
+      </CustomButton>
+      <Modal
+        visible={isModalVisible}
+        onCancel={handleCancel}
+        footer={[
+          <Button className={styles.secondary} onClick={handleCancel} key="cancel">
+            Batal
+          </Button>,
+          <Button className={styles.primary} onClick={handleOk} key="ok">
+            {data.cta}
+          </Button>,
+        ]}
+      >
+        <Row>
+          <Col span={24} className={styles.container}>
+            <img src={WarningIcon} alt="warning" style={{width: "48px"}} />
+            <div className={styles.separator}>
+              <span className={`h4-sb`}>{data.title}</span>
+              <p>{data.desc}</p>
+            </div>
+          </Col>
+        </Row>
+      </Modal>
+    </>
+  );
 }
