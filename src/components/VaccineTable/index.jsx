ec4d2b1d4b56177c777a8355baaec6ad00f4be59
TakayumAja <asraftakayuma76@gmail.com>
2022-07-13 17:05:40 +0700

    cant use fitur crud vaccine - feature/vaccine-admin

--- src/components/VaccineTable/index.jsx
+++ src/components/VaccineTable/index.jsx
@@ -3,9 +3,17 @@ import React, { useState, useEffect } from "react";
 
 /** Components */
 import VaccineModalAdd from "../VaccineModalAdd";
+import VaccineModalEdit from "../VaccineModalEdit";
+import VaccineModalDelete from "../VaccineModalDelete";
+
+/** Axios */
+import axiosInstance from "../../networks/apis";
+
+/** Auth */
+import { getUserId } from "../../utils/helpers/Auth";
 
 /** Antd Design */
-import { Form, Input, Button, Table } from "antd";
+import { Form, Input, Button, Table, Col, Row } from "antd";
 import Column from "antd/lib/table/Column";
 
 /** Antd Design Icons */
@@ -13,17 +21,13 @@ import { SearchOutlined } from "@ant-design/icons";
 
 /** Style */
 import style from "./VaccineTable.module.css";
-import VaccineModalEdit from "../VaccineModalEdit";
-import VaccineModalDelete from "../VaccineModalDelete";
-import axiosInstance from "../../networks/apis";
-import { getUserId } from "../../utils/helpers/Auth";
 
 const VaccineTable = () => {
   const [rawData, setRawData] = useState([]);
   const [data, setData] = useState([]);
+  const [tableData, setTableData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [refetchToggle, setRefetchToggle] = useState(false);
-  const [tableData, setTableData] = useState([]);
 
   useEffect(() => {
     setLoading(true);
@@ -32,6 +36,7 @@ const VaccineTable = () => {
       .then((response) => {
         setLoading(false);
         setRawData(response.data.data.filter((item) => item.facility.profile.user_id === getUserId()));
+        // console.log(response.data.data.filter((item) => item.facility.profile.user_id === getUserId() && item.stock >= 0));
       })
       .catch((error) => {
         setLoading(false);
@@ -41,44 +46,44 @@ const VaccineTable = () => {
 
   useEffect(() => {
     const filteredData = rawData.map((value) => {
-      return {
-        id: value.vaccine.id,
-        vaccine_name: value.vaccine.vaccine_name,
-        stock: value.stock,
-      };
+      return { id: value.vaccine.id, vaccine_name: value.vaccine.vaccine_name, stock: value.stock };
     });
+
     setData(filteredData);
     setTableData(filteredData);
   }, [rawData]);
 
-  console.log(tableData);
-
   const search = (values) => {
     const lowerCaseValue = values.keyword.toLowerCase().trim();
     const filteredData = data.filter((value) => Object.keys(value).some((key) => value[key].toString().toLowerCase().includes(lowerCaseValue)));
-
     setTableData(filteredData);
   };
 
   return (
     <>
-      <div className={style.searchWrapper}>
-        <VaccineModalAdd setRefetchToggle={setRefetchToggle} refetchToggle={refetchToggle} />
+      <Row justify="space-between" align="middle">
+        <Col xs={24} md={12}>
+          <VaccineModalAdd setRefetchToggle={setRefetchToggle} refetchToggle={refetchToggle} />
+        </Col>
+        <Col xs={24} md={6}>
+          <div className={style.searchWrapper}>
+            <Form className={style.searchForm} onFinish={search}>
+              <Form.Item name="keyword">
+                <Input type="search" placeholder="Search" className={style.search} />
+              </Form.Item>
+              <Form.Item>
+                <Button htmlType="submit">
+                  <SearchOutlined />
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+        </Col>
+      </Row>
 
-        <Form className={style.searchForm} onFinish={search}>
-          <Form.Item name="keyword">
-            <Input type="search" placeholder="Search" className={style.search} />
-          </Form.Item>
-          <Form.Item>
-            <Button htmlType="submit">
-              <SearchOutlined />
-            </Button>
-          </Form.Item>
-        </Form>
-      </div>
       <div className={style.tableWrapper}>
         <Table dataSource={tableData} pagination={{ position: ["bottomCenter"] }} scroll={{ x: 240 }} rowKey="id" loading={loading}>
-          <Column title="Id Sesi" dataIndex="id" key="id" />
+          <Column title="Id Vaksin" dataIndex="id" key="id" />
           <Column title="Jenis Vaksin" dataIndex="vaccine_name" key="vaccine_name" />
           <Column title="Stock" dataIndex="stock" key="stock" />
           <Column
