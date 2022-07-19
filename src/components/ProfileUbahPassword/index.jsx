2f4cb2f2489eff355c2e89842abb3384a9abb05a
TakayumAja <asraftakayuma76@gmail.com>
2022-07-20 02:58:23 +0700

    add fitur forgetpassword feature/profile-citizen

--- src/components/ProfileUbahPassword/index.jsx
+++ src/components/ProfileUbahPassword/index.jsx
@@ -1,18 +1,57 @@
 import { Form, Row, Col, Input } from "antd";
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
+import axiosInstance from "../../networks/apis";
+import { getUserId } from "../../utils/helpers/Auth";
 import CustomButton from "../CustomButton";
 import style from "./ProfileUbahPassword.module.css";
 
 const ProfileUbahPassword = () => {
   const [form] = Form.useForm();
+  const [dataUser, setDataUser] = useState([]);
+
+  useEffect(() => {
+    axiosInstance
+      .get(`/user/${getUserId()}`, { data: "" })
+      .then((response) => {
+        setDataUser(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  console.log(dataUser);
 
   const onFinish = (values) => {
+    const inputData = {
+      username: dataUser.username,
+      name: dataUser.name,
+      email: dataUser.email,
+      password: values.confirmpassword,
+      profile: {
+        role: dataUser.profile.role,
+      },
+    };
+
+    axiosInstance
+      .put(`/user/${getUserId()}`, inputData)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    console.log(inputData);
     console.log(values);
   };
 
   const onFinishFailed = (error) => {
     console.log(error);
   };
+
+  console.log(dataUser.password);
   return (
     <>
       <div className={style.title}>
@@ -21,7 +60,7 @@ const ProfileUbahPassword = () => {
       <Row>
         <Col span={10}>
           <Form name="ubahpassword" layout="vertical" form={form} requiredMark={false} onFinish={onFinish} onFinishFailed={onFinishFailed}>
-            <Form.Item
+            {/* <Form.Item
               name="passwordlama"
               label="Password Lama"
               rules={[
@@ -32,7 +71,7 @@ const ProfileUbahPassword = () => {
               ]}
             >
               <Input.Password className="input" placeholder="Masukkan Password Lama" />
-            </Form.Item>
+            </Form.Item> */}
             <Form.Item
               name="passwordbaru"
               label="Password Baru"
