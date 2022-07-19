e7f20a9b6f051c3fff4a0740b8481aacbf78bcde
TakayumAja <asraftakayuma76@gmail.com>
2022-07-19 23:44:15 +0700

    add edit profile feature/profile-citizen

--- src/components/ProfileSaya/index.jsx
+++ src/components/ProfileSaya/index.jsx
@@ -1,30 +1,31 @@
 import { Col, Row } from "antd";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import axiosInstance from "../../networks/apis";
+import { getUserId } from "../../utils/helpers/Auth";
 import CustomButton from "../CustomButton";
 
 import style from "./ProfileSaya.module.css";
 
 const ProfileSaya = () => {
-  const [data, setData] = useState({
-    nama: "Xavier Davis Chaniago",
-    nik: "1234567890987654321",
-    tempatLahir: "1234567890987654321",
-    tanggalLahir: "09 September 2022",
-    jenisKelamin: "Laki - Laki",
-    status: "Anak Tengah",
-    email: "Xavierblabla@gmail.com",
-    noHp: "089898989898",
-    alamatKtp: "Perumahan Griya Permai, Blok B1 No.1, Jl. Diponegoro, Bandung.",
-    alamatSekarang: "Perumahan Griya Permai, Blok B1 No.1, Jl. Diponegoro, Bandung.",
-  });
+  const [dataUser, setDataUser] = useState([]);
+
+  useEffect(() => {
+    axiosInstance
+      .get(`/family/${getUserId()}`, { data: "" })
+      .then((response) => {
+        setDataUser(response.data.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   const navigate = useNavigate();
 
   const handleEdit = (id, callbackData) => {
-    console.log(id);
     console.log("sent state : ", callbackData);
-    navigate(`../editprofile/${id}`, { state: { data } });
+    navigate(`../editprofile/${id}`, { state: { dataUser } });
   };
   return (
     <>
@@ -33,53 +34,53 @@ const ProfileSaya = () => {
       </div>
       <div className={style.label}>
         <p>Nama</p>
-        <h3>{data.nama}</h3>
+        <h3>{dataUser.name}</h3>
       </div>
       <div className={style.label}>
         <p>NIK</p>
-        <h3>{data.nik}</h3>
+        <h3>{dataUser.nik}</h3>
       </div>
       <Row justify="space-between">
         <Col>
           <div className={style.label}>
             <p>Tempat Lahir</p>
-            <h3>{data.tempatLahir}</h3>
+            <h3>{dataUser.place_of_birth}</h3>
           </div>
         </Col>
         <Col>
           <div className={style.label}>
             <p>Tanggal Lahir</p>
-            <h3>{data.tanggalLahir}</h3>
+            <h3>{dataUser.date_of_birth}</h3>
           </div>
         </Col>
       </Row>
       <div className={style.label}>
         <p>Jenis Kelamin</p>
-        <h3>{data.jenisKelamin}</h3>
+        <h3>{dataUser.gender}</h3>
       </div>
       <div className={style.label}>
         <p>Status dalam Keluarga</p>
-        <h3>{data.status}</h3>
+        <h3>{dataUser.status_in_family}</h3>
       </div>
       <div className={style.label}>
         <p>Email</p>
-        <h3>{data.email}</h3>
+        <h3>{dataUser.email}</h3>
       </div>
       <div className={style.label}>
         <p>No.HP</p>
-        <h3>{data.noHp}</h3>
+        <h3>{dataUser.phone_number}</h3>
       </div>
       <div className={style.label}>
         <p>Alamat Rumah di KTP</p>
-        <h3>{data.alamatKtp}</h3>
+        <h3>{dataUser.id_card_address}</h3>
       </div>
       <div className={style.label}>
         <p>Alamat Rumah Saat ini</p>
-        <h3>{data.alamatSekarang}</h3>
+        <h3>{dataUser.residence_address}</h3>
       </div>
       <Row className={style.btnEdit}>
         <Col span={24}>
-          <CustomButton variant="primary" block="true" onClick={() => handleEdit(1, data)}>
+          <CustomButton variant="primary" block="true" onClick={() => handleEdit(dataUser.id, dataUser)}>
             Edit Profile
           </CustomButton>
         </Col>
